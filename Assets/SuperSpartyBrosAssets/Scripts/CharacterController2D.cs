8a516f9162e6022d6dc2c3d25e12ef1adb64b48e
Arda KAPTAN <arda_kaptan@hotmail.com>
2020-10-14 14:05:18 +0300

    menu and
    more

--- Assets/SuperSpartyBrosAssets/Scripts/CharacterController2D.cs
+++ Assets/SuperSpartyBrosAssets/Scripts/CharacterController2D.cs
@@ -291,4 +291,9 @@ public class CharacterController2D : MonoBehaviour {
 		_transform.position = spawnloc;
 		_animator.SetTrigger("Respawn");
 	}
+
+	public void EnemyBounce()
+	{
+		DoJump();
+	}
 }
